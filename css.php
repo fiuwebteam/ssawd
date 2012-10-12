7d892b4456fb0474728d3963e7368a2539f0be03
Andre <amolive@fiu.edu>
2012-10-12 15:45:00 -0400

    Added function to image.php to only read files from the same host by default.

---
+++ css.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+ * SSAWD css.php v 1.0
+ * Author: Andre Oliveira
+ * Date: 10/12/12
+ * Link: https://github.com/fiuwebteam/ssawd
+ * 
+ * This is a file to 
+ * 
+ */
+
+?>
--- image.php
+++ image.php
@@ -5,20 +5,35 @@
  * Date: 10/11/12
  * Link: https://github.com/fiuwebteam/ssawd
  * 
- * This is a file to automatically resize and cache images depending on the 
- * device viewing the site.
+ * This is a file to detect the device currently being used, 
+ * read the passed image, and, depending on the device,
+ * resize and cache local copies of the image.
  * 
  */
 
 // Set the width of the devices (in pixels) here.
 $mobileWidth = 480;
 $tabletWidth = 1024;
+// Specify if the image files should only read on his host or not. The default is local.
+$localImagesOnly = true;
 
+// Read the passed image, exit if none is given.
 $imageLocation = isset($_GET["img"]) ? $_GET["img"] : null;
 if ($imageLocation == null) { 
 	die("You need to specify the location of the image."); 
 }
-
+// Makes sure that the images are from the same host only.
+if ($localImagesOnly) {
+	$imgLocParse = parse_url($imageLocation);
+	if (isset($imgLocParse["host"]) && $_SERVER["HTTP_HOST"] != $imgLocParse["host"]) {
+		die("This script is currently only able to use images on the same host.<br/>
+		This can be changed in the configuration.");
+	}
+}
+/*
+ *  Read the device type cookie if available, else, parse the request,
+ *  identify the device, and save the cookie for future use.
+*/
 if ( isset($_COOKIE["device_type"]) ) {
 	$type = $_COOKIE["device_type"];	
 } else {
@@ -33,12 +48,12 @@ if ( isset($_COOKIE["device_type"]) ) {
 	setcookie("device_type", $type, strtotime('+30 days') );
 }
 
-require('./lib/SimpleImage.php');
-
 $imageName = "./cache/images/" . sha1($imageLocation . $type);
 
+require('./lib/SimpleImage.php');
 $image = new SimpleImage();
 
+// Load the local copy is present, else, resize if appropriate and save a local copy.
 if (file_exists($imageName)) {
 	$image->load($imageName);	
 } else {
@@ -60,7 +75,7 @@ if (file_exists($imageName)) {
 	if ($width) { 
 		$image->resizeToWidth($width); 
 	}
-	if (!$image->save($imageName)) {
+	if (!$image->save($imageName, $image->image_type)) {
 		die("Could not save local cache.");
 	}	
 }
